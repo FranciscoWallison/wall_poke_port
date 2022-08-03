c89a77dac5eeabae91dd1b135f277697e72001aa
Francisco Wallison <franciscowallison@gmail.com>
2022-08-03 10:12:45 -0300

    iniciando novos eventos de chat

--- js/chat.js
+++ js/chat.js
@@ -4,9 +4,14 @@ let interval_chat;
 let index_chat = 0;
 
 function typewriter() {
-  if (index_chat >= text_dialog_chat.length) {
 
+  if (index_chat >= text_dialog_chat.length) {
+    // para o evento do setInterval
     clearInterval(interval_chat);
+    // start do movimentos de certas
+    moving = true;
+    lastKeyPortal = false;
+    lastKeyChat = true;
   } else {
     textChat.append(text_dialog_chat[index_chat]);
     index_chat++;
