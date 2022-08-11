de43342f3d471056671d4754d58deb91e1011108
Francisco Wallison <franciscowallison@gmail.com>
2022-08-11 12:14:00 -0300

    init chat e speed button

--- js/button_events.js
+++ js/button_events.js
@@ -33,7 +33,7 @@ function onPressEvent(moving) {
   }
 }
 
-function b_button(player, characters, boundaries, movables, moving) {
+async function b_button(player, characters, boundaries, movables, moving) {
 
   let checkNpcUp = checkForCharacterCollision({
     characters,
@@ -79,16 +79,11 @@ function b_button(player, characters, boundaries, movables, moving) {
       document.querySelector('#showcase_chat').style.display = 'block';
       const textChatTitle = document.getElementById('text-chat-title');
       text_dialog_chat = valid_type.type.text;
-      textChatTitle.append(valid_type.type.title);      
-      let speed = 200;
-
-      if (keys.z.pressed) {
-        speed = 1600;
-      }
+      textChatTitle.append(valid_type.type.title);
 
       textChatTitle.innerHTML = valid_type.type.title;
       // TODO:: AO CLICLAR NO BTN FICAR MAIS RAPIDO O DIALOGO DO CHAT
-      interval_chat = setInterval(typewriter, speed);
+      await typewriter();
 
     }
   }
